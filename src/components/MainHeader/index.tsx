6fab46ff9db60daa5f3778cc9660d76cc9026649
laisescorcio <lais.correia@outlook.com>
2022-01-18 22:23:17 -0300

    HEADER - Nome e Emoji

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useMemo } from 'react';
+
+import emojis from '../../utils/emojis';
+
+import { 
+    Container, 
+    Profile, 
+    Welcome, 
+    UserName 
+} from './styles';
 
 const MainHeader: React.FC = () => {
+    const emoji = useMemo(() => {
+        const indice = Math.floor(Math.random() * emojis.length)
+
+        return emojis[indice];
+    }, [])
+
     return (
         <Container>
-            <h1>MainHeader</h1>
+            <h1>Toggle</h1>
+
+            <Profile>
+                <Welcome>Olá, {emoji}</Welcome>
+                <UserName>Laís Escorcio Correia!</UserName>
+            </Profile>
         </Container>
     )
 }
