4bd219517f371af0511d674c29fde5178584a2a1
laisescorcio <lais.correia@outlook.com>
2022-03-07 18:46:05 -0300

    ENTRADA E SAÍDA - Renderização e Estilização

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,75 +1,108 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState, useEffect } from "react";
 
-import ContentHeader from '../../components/ContentHeader';
-import SelectInput from '../../components/SelectInput';
-import HistoryFinanceCard from '../../components/HistoryFinanceCard';
+import ContentHeader from "../../components/ContentHeader";
+import SelectInput from "../../components/SelectInput";
+import HistoryFinanceCard from "../../components/HistoryFinanceCard";
 
-import { Container, Content, Filters } from './style'
+import { Container, Content, Filters } from "./style";
+
+import gains from "../../repositories/gains";
+import expenses from "../../repositories/expenses";
 
 interface IRouteParams {
-    match: { //informacoes da rota
-        params: { // parametros da rota
-            type: string; // meu type
-        }
-    }
+  match: {
+    //informacoes da rota
+    params: {
+      // parametros da rota
+      type: string; // meu type
+    };
+  };
+}
+
+interface IData {
+  id: string;
+  description: string;
+  amountFormatted: string;
+  frequency: string;
+  dateFormatted: string;
+  tagColor: string;
 }
 
 const List: React.FC<IRouteParams> = ({ match }) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [data, setData] = useState<IData[]>([]);
 
-    const { type } = match.params;
+  const { type } = match.params;
 
-    const title = useMemo(() => {
-        return type === 'entry-balance' ? 'Entradas' : 'Saídas'
-    }, [ type ])
+  const title = useMemo(() => {
+    return type === "entry-balance" ? "Entradas" : "Saídas";
+  }, [type]);
 
-    const lineColor = useMemo(() => {
-        return type === 'entry-balance' ? '#F7931B' : '#E44C4E'
-    }, [ type ])
+  const lineColor = useMemo(() => {
+    return type === "entry-balance" ? "#F7931B" : "#E44C4E";
+  }, [type]);
 
-    const months = [
-        { value: 7, label: "Julho" },
-        { value: 8, label: "Agosto" },
-        { value: 9, label: "Setembro" },
-    ];
+  const listData = useMemo(() => {
+    return type === "entry-balance" ? gains : expenses;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    const years = [
-        { value: 2020, label: 2020 },
-        { value: 2019, label: 2019 },
-        { value: 2018, label: 2018 },
-    ];
+  const months = [
+    { value: 7, label: "Julho" },
+    { value: 8, label: "Agosto" },
+    { value: 9, label: "Setembro" },
+  ];
 
-    return (
-        <Container>
-            <ContentHeader title={title} lineColor={lineColor}>
-                <SelectInput options={months} />
-                <SelectInput options={years} />
-            </ContentHeader>
+  const years = [
+    { value: 2020, label: 2020 },
+    { value: 2019, label: 2019 },
+    { value: 2018, label: 2018 },
+  ];
 
-            <Filters>
-                <button 
-                    type="button"
-                    className="tag-filter tag-filter-recurrent"
-                >
-                    Recorrentes
-                </button>
-                <button 
-                    type="button"
-                    className="tag-filter tag-filter-eventual"
-                >
-                    Eventuais
-                </button>
-            </Filters>
+  useEffect(() => {
+    const response = listData.map((item) => {
+      return {
+        id: String(Math.random() * data.length),
+        description: item.description,
+        amountFormatted: item.amount,
+        frequency: item.frequency,
+        dateFormatted: item.date,
+        tagColor: item.frequency === "recorrente" ? "#4E41F0" : "#E44C4E",
+      };
+    });
+    setData(response);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-            <Content>
-                <HistoryFinanceCard  
-                    tagColor='#E44C4E' 
-                    title='Conta de Luz' 
-                    subtitle='31/07/2020'
-                    amount='R$ 130,00'
-                />                   
-            </Content>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <ContentHeader title={title} lineColor={lineColor}>
+        <SelectInput options={months} />
+        <SelectInput options={years} />
+      </ContentHeader>
+
+      <Filters>
+        <button type="button" className="tag-filter tag-filter-recurrent">
+          Recorrentes
+        </button>
+        <button type="button" className="tag-filter tag-filter-eventual">
+          Eventuais
+        </button>
+      </Filters>
+
+      <Content>
+        {data.map((item) => (
+          <HistoryFinanceCard
+            key={item.id}
+            tagColor={item.tagColor}
+            title={item.description}
+            subtitle={item.dateFormatted}
+            amount={item.amountFormatted}
+          />
+        ))}
+      </Content>
+    </Container>
+  );
+};
 
 export default List;
