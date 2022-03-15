3a7ea4475400f34c6233a45482070503907baf87
laisescorcio <lais.correia@outlook.com>
2022-03-14 22:39:21 -0300

    SignIn - criando component e inicio da estilizacao

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,27 @@
 import React from "react";
 
+import logoImg from "../../assets/logo.svg";
+
+import { Container, Logo, Form, FormTitle } from "./styles";
+
 // stateless component: componente sem estado, não precisa de 'return {}'
-const SignIn: React.FC = () => <h1>Sign In</h1>;
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Logo>
+        <img src={logoImg} alt="Minha Carteira" />
+        <h2>Minha Carteira</h2>
+      </Logo>
+
+      <Form>
+        <FormTitle>Entrar</FormTitle>
+        <input type="text"></input>
+        <input type="text"></input>
+
+        <button type="submit">Acessar</button>
+      </Form>
+    </Container>
+  );
+};
 
 export default SignIn;
