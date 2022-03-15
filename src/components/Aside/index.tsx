eb2a1e342824fc9ac0b7dd09521590ca861a7331
laisescorcio <lais.correia@outlook.com>
2022-03-15 19:33:21 -0300

    SignOut: aplicacao da função de saída do login

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import logoImg from "../../assets/logo.svg";
 
+import { useAuth } from "../../hooks/auth";
+
 import {
   MdDashboard,
   MdArrowDownward,
@@ -16,38 +18,42 @@ import {
   Title,
   MenuContainer,
   MenuItemLink,
+  MenuItemButton,
 } from "./styles";
 
-// stateless component: componente sem estado, não precisa de 'return {}'
-const Aside: React.FC = () => (
-  <Container>
-    <Header>
-      <LogoImg src={logoImg} alt="Logo Minha Carteira" />
-      <Title>Minha Carteira</Title>
-    </Header>
+const Aside: React.FC = () => {
+  const { signOut } = useAuth();
 
-    <MenuContainer>
-      <MenuItemLink href="/">
-        <MdDashboard />
-        Dashboard
-      </MenuItemLink>
+  return (
+    <Container>
+      <Header>
+        <LogoImg src={logoImg} alt="Logo Minha Carteira" />
+        <Title>Minha Carteira</Title>
+      </Header>
 
-      <MenuItemLink href="/list/entry-balance">
-        <MdArrowUpward />
-        Entradas
-      </MenuItemLink>
+      <MenuContainer>
+        <MenuItemLink href="/">
+          <MdDashboard />
+          Dashboard
+        </MenuItemLink>
 
-      <MenuItemLink href="/list/exit-balance">
-        <MdArrowDownward />
-        Saídas
-      </MenuItemLink>
+        <MenuItemLink href="/list/entry-balance">
+          <MdArrowUpward />
+          Entradas
+        </MenuItemLink>
 
-      <MenuItemLink href="#">
-        <MdExitToApp />
-        Sair
-      </MenuItemLink>
-    </MenuContainer>
-  </Container>
-);
+        <MenuItemLink href="/list/exit-balance">
+          <MdArrowDownward />
+          Saídas
+        </MenuItemLink>
+
+        <MenuItemButton onClick={signOut}>
+          <MdExitToApp />
+          Sair
+        </MenuItemButton>
+      </MenuContainer>
+    </Container>
+  );
+};
 
 export default Aside;
